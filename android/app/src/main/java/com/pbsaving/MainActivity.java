25bde2c5f8f517b7f49861c397862d2b6c2bbae7
Rian Divayana <rian.divayana@gmail.com>
2022-03-29 13:36:06 +0800

    checkpoint

--- android/app/src/main/java/com/pbsaving/MainActivity.java
+++ android/app/src/main/java/com/pbsaving/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "PBSaving";
+    return "Otomedia";
   }
 
   @Override
