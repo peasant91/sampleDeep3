c332fd2b99028b10279fe702f80d6c98b9eae990
sumariana <42775471+sumariana@users.noreply.github.com>
2022-09-27 15:07:36 +0800

    release

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,6 +1,6 @@
 export default {
     isMockDesign: false,
-    isDevMode: true,
+    isDevMode: false,
     minimumTimeToSend: -30, //-1 = 1 minutes
     cardList: ['ktp', 'sim_a', 'sim_b'],
     developmentMode: 'live', //dev | live | staging
